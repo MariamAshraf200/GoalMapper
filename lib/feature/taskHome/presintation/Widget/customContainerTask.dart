335000cb076345f6e8f6e4ef508ab01877dceb44
MariamAshraf200 <mariamsarhan201@gmail.com>
2024-12-08 03:07:46 +0200

    Update by long press

--- lib/feature/taskHome/presintation/Widget/customContainerTask.dart
+++ lib/feature/taskHome/presintation/Widget/customContainerTask.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import '../../domain/entity/taskEntity.dart';
 import '../bloc/bloc.dart';
 import '../bloc/event.dart';
+import '../screen/AddNewTaskScreen.dart';
 
 class TaskCard extends StatelessWidget {
   final String taskId;
@@ -33,132 +34,152 @@ class TaskCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8.0),
-      padding: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            blurRadius: 6.0,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Text(
-                  title,
-                  style: const TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
+    return GestureDetector(
+      onLongPress: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => AddTaskScreen(
+              existingTask: TaskEntity(
+                id: taskId,
+                title: title,
+                description: description,
+                date: date,
+                time: time,
+                priority: priority,
+                status: '',
               ),
-              Row(
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.black,
-                      shape: BoxShape.circle,
-                    ),
-                    child: IconButton(
-                      onPressed: onViewClicked,
-                      icon: const Icon(
-                        CupertinoIcons.arrow_up_right,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 8.0),
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.red,
-                      shape: BoxShape.circle,
-                    ),
-                    child: IconButton(
-                      onPressed: () => _deleteTask(context),
-                      icon: const Icon(
-                        Icons.delete,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(height: 8.0),
-          Text(
-            description,
-            style: const TextStyle(
-              fontSize: 14.0,
-              color: Colors.grey,
             ),
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
           ),
-          const SizedBox(height: 16.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  const Icon(
-                    Icons.calendar_today,
-                    size: 16.0,
-                    color: Colors.grey,
-                  ),
-                  const SizedBox(width: 4.0),
-                  Text(
-                    date,
-                    style: const TextStyle(fontSize: 12.0, color: Colors.grey),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.access_time,
-                    size: 16.0,
-                    color: Colors.grey,
-                  ),
-                  const SizedBox(width: 4.0),
-                  Text(
-                    time,
-                    style: const TextStyle(fontSize: 12.0, color: Colors.grey),
-                  ),
-                ],
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 12.0,
-                  vertical: 6.0,
-                ),
-                decoration: BoxDecoration(
-                  color: priorityColor,
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                child: Text(
-                  priority,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 12.0,
-                    fontWeight: FontWeight.bold,
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8.0),
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              blurRadius: 6.0,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
                   ),
                 ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                        shape: BoxShape.circle,
+                      ),
+                      child: IconButton(
+                        onPressed: onViewClicked,
+                        icon: const Icon(
+                          CupertinoIcons.arrow_up_right,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8.0),
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+                      ),
+                      child: IconButton(
+                        onPressed: () => _deleteTask(context),
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              description,
+              style: const TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey,
               ),
-            ],
-          ),
-        ],
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            const SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.calendar_today,
+                      size: 16.0,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(width: 4.0),
+                    Text(
+                      date,
+                      style: const TextStyle(fontSize: 12.0, color: Colors.grey),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.access_time,
+                      size: 16.0,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(width: 4.0),
+                    Text(
+                      time,
+                      style: const TextStyle(fontSize: 12.0, color: Colors.grey),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12.0,
+                    vertical: 6.0,
+                  ),
+                  decoration: BoxDecoration(
+                    color: priorityColor,
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  child: Text(
+                    priority,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/feature/taskHome/presintation/screen/todoScreen.dart
+++ lib/feature/taskHome/presintation/screen/todoScreen.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
 import '../../domain/entity/taskEntity.dart';
 import '../Widget/customContainerTask.dart';
 
 class TaskListScreen extends StatelessWidget {
   final List<TaskEntity> tasks; // Accept tasks as a parameter.
 
-  // Constructor to pass tasks
-   TaskListScreen({Key? key, required this.tasks}) : super(key: key);
-  final uuid = Uuid();
+  TaskListScreen({Key? key, required this.tasks}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String uniqueTaskId = uuid.v4();
-
     return Scaffold(
       body: ListView(
         padding: const EdgeInsets.all(16.0),
@@ -30,21 +25,20 @@ class TaskListScreen extends StatelessWidget {
 
           // Iterate through the tasks and display each one
           ...tasks.map((task) => TaskCard(
-            title: task.title,
-            description: task.description,
-            date: task.date,
-            time: task.time,
-            priority: task.priority,
-            priorityColor: _getPriorityColor(task.priority), onViewClicked: () {  }, taskId: uniqueTaskId, // Example of how to assign a color based on priority
-
-
-          )),
+                title: task.title,
+                description: task.description,
+                date: task.date,
+                time: task.time,
+                priority: task.priority,
+                priorityColor: _getPriorityColor(task.priority),
+                onViewClicked: () {},
+                taskId: task.id,
+              )),
         ],
       ),
     );
   }
 
-  // Helper function to return priority color based on task priority
   Color _getPriorityColor(String priority) {
     switch (priority) {
       case 'High':
