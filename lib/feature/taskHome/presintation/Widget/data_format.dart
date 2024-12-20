d9886f75d3c3e11e0ccfcfa81d90422b11480faa
yousef omar <yousifomar698@gmail.com>
2024-12-21 00:57:17 +0200

    "TaskTrack Screen UI first handle "

--- lib/feature/taskHome/presintation/Widget/data_format.dart
+++ lib/feature/taskHome/presintation/Widget/data_format.dart
@@ -1,17 +1,136 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:mapper_app/core/constants/app_colors.dart';
 
 import '../../../../core/customColor.dart';
 
+// class DataFormat extends StatefulWidget {
+//   final String selectedDate;
+//   final Function(DateTime) onDateSelected;
+//
+//   const DataFormat({
+//     super.key,
+//     required this.selectedDate,
+//     required this.onDateSelected,
+//   });
+//
+//   @override
+//   State<DataFormat> createState() => _DataFormatState();
+// }
+//
+// class _DataFormatState extends State<DataFormat> {
+//   late DateTime currentDate;
+//   late List<DateTime> weekDays;
+//   CustomColor color = CustomColor();
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     currentDate = DateTime.now();
+//     weekDays = List.generate(
+//       7,
+//           (index) => currentDate.add(Duration(days: index - currentDate.weekday + 1)),
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Padding(
+//           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               Text(
+//                 DateFormat('MMMM').format(currentDate),
+//                 style: TextStyle(
+//                   fontSize: 18,
+//                   fontWeight: FontWeight.bold,
+//                   color: color.secondaryColor,
+//                 ),
+//               ),
+//               // Weekday and Date Row
+//               Center(
+//                 child: Padding(
+//                   padding: const EdgeInsets.symmetric(vertical: 12.0),
+//                   child: Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                     children: weekDays.map((date) {
+//                       bool isToday = date.day == currentDate.day &&
+//                           date.month == currentDate.month &&
+//                           date.year == currentDate.year;
+//
+//                       bool isSelected = widget.selectedDate ==
+//                           DateFormat('yyyy-MM-dd').format(date);
+//
+//                       return GestureDetector(
+//                         onTap: () {
+//                           widget.onDateSelected(date);
+//                         },
+//                         child: Column(
+//                           mainAxisSize: MainAxisSize.min,
+//                           children: [
+//                             Text(
+//                               DateFormat('EEE').format(date), // Weekday (e.g., Mon, Tue)
+//                               style: TextStyle(
+//                                 fontSize: 14,
+//                                 fontWeight: FontWeight.bold,
+//                                 color: isSelected
+//                                     ? color.basicColor
+//                                     : isToday
+//                                     ? color.secondaryColor
+//                                     : color.primaryColor,
+//                               ),
+//                             ),
+//                             const SizedBox(height: 4),
+//                             Container(
+//                               decoration: BoxDecoration(
+//                                 color: isSelected
+//                                     ? color.secondaryColor
+//                                     : isToday
+//                                     ? color.secondaryColor
+//                                     : color.additionalColor,
+//                                 shape: BoxShape.circle,
+//                               ),
+//                               padding: const EdgeInsets.all(10),
+//                               child: Text(
+//                                 DateFormat('d').format(date),
+//                                 style: TextStyle(
+//                                   fontSize: 14,
+//                                   fontWeight: FontWeight.bold,
+//                                   color: isSelected
+//                                       ? color.basicColor
+//                                       : isToday
+//                                       ? color.basicColor
+//                                       : color.primaryColor,
+//                                 ),
+//                               ),
+//                             ),
+//                           ],
+//                         ),
+//                       );
+//                     }).toList(),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
 class DataFormat extends StatefulWidget {
   final String selectedDate;
   final Function(DateTime) onDateSelected;
 
   const DataFormat({
-    Key? key,
+    super.key,
     required this.selectedDate,
     required this.onDateSelected,
-  }) : super(key: key);
+  });
 
   @override
   State<DataFormat> createState() => _DataFormatState();
@@ -20,6 +139,7 @@ class DataFormat extends StatefulWidget {
 class _DataFormatState extends State<DataFormat> {
   late DateTime currentDate;
   late List<DateTime> weekDays;
+  late ScrollController _scrollController;
   CustomColor color = CustomColor();
 
   @override
@@ -30,94 +150,100 @@ class _DataFormatState extends State<DataFormat> {
       7,
           (index) => currentDate.add(Duration(days: index - currentDate.weekday + 1)),
     );
+    _scrollController = ScrollController();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _centerSelectedDate();
+    });
+  }
+
+  @override
+
+  void didUpdateWidget(covariant DataFormat oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _centerSelectedDate();
+    });
+  }
+
+  void _centerSelectedDate() {
+    int selectedIndex = weekDays.indexWhere((date) =>
+    widget.selectedDate == DateFormat('dd/MM/yyyy').format(date));
+    if (selectedIndex != -1) {
+      double screenWidth = MediaQuery.of(context).size.width;
+      double itemWidth = 56.0; // Estimated width of each item including padding
+      double scrollOffset = (selectedIndex * itemWidth) - (screenWidth / 2) + (itemWidth / 2);
+
+      _scrollController.animateTo(
+        scrollOffset.clamp(
+          _scrollController.position.maxScrollExtent,
+          _scrollController.position.maxScrollExtent,
+        ),
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                DateFormat('MMMM').format(currentDate),
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: color.secondaryColor,
-                ),
-              ),
-              // Weekday and Date Row
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: SingleChildScrollView(
+        controller: _scrollController,
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: weekDays.map((date) {
+            bool isSelected = widget.selectedDate ==
+                DateFormat('dd/MM/yyyy').format(date);
+
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0), // Add spacing between items
+              child: GestureDetector(
+                onTap: () {
+                  widget.onDateSelected(date);
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: isSelected ? AppColors.secondaryColor : AppColors.secondBackgroundColor,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: weekDays.map((date) {
-                      bool isToday = date.day == currentDate.day &&
-                          date.month == currentDate.month &&
-                          date.year == currentDate.year;
-
-                      bool isSelected = widget.selectedDate ==
-                          DateFormat('yyyy-MM-dd').format(date);
-
-                      return GestureDetector(
-                        onTap: () {
-                          widget.onDateSelected(date);
-                        },
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              DateFormat('EEE').format(date), // Weekday (e.g., Mon, Tue)
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: isSelected
-                                    ? color.basicColor
-                                    : isToday
-                                    ? color.secondaryColor
-                                    : color.primaryColor,
-                              ),
-                            ),
-                            const SizedBox(height: 4),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: isSelected
-                                    ? color.secondaryColor
-                                    : isToday
-                                    ? color.secondaryColor
-                                    : color.additionalColor,
-                                shape: BoxShape.circle,
-                              ),
-                              padding: const EdgeInsets.all(10),
-                              child: Text(
-                                DateFormat('d').format(date),
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: isSelected
-                                      ? color.basicColor
-                                      : isToday
-                                      ? color.basicColor
-                                      : color.primaryColor,
-                                ),
-                              ),
-                            ),
-                          ],
+                    children: [
+                      Text(
+                        DateFormat('EEE').format(date), // Weekday
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: isSelected ? Colors.white : Colors.black,
                         ),
-                      );
-                    }).toList(),
+                      ),
+                      const SizedBox(height: 4),
+                      if (isSelected)
+                        Text(
+                          DateFormat(',d MMM').format(date), // Day and Month
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: isSelected ? Colors.white : Colors.black ,
+                          ),
+                        ),
+                    ],
                   ),
                 ),
               ),
-            ],
-          ),
+            );
+          }).toList(),
         ),
-      ],
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
--- lib/feature/taskHome/presintation/screen/taskTrack.dart
+++ lib/feature/taskHome/presintation/screen/taskTrack.dart
@@ -1,7 +1,12 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import '../../../../core/constants/app_colors.dart';
+import '../../../../core/constants/app_spaces.dart';
 import '../../../../core/customColor.dart';
+import '../../../../core/util/widgets/custom_card.dart';
 import '../Widget/customContainerTask.dart';
 import '../Widget/data_format.dart';
 import '../bloc/bloc.dart';
@@ -10,14 +15,15 @@ import '../bloc/event.dart';
 import 'AddNewAndUpdateTaskScreen.dart';
 
 class TaskTrack extends StatefulWidget {
-   const TaskTrack({super.key});
+  const TaskTrack({super.key});
+
   @override
   _TaskTrackState createState() => _TaskTrackState();
 }
 
 class _TaskTrackState extends State<TaskTrack> {
   late String selectedDate;
-  CustomColor color = CustomColor();
+  // CustomColor color = CustomColor();
 
   @override
   void initState() {
@@ -37,21 +43,36 @@ class _TaskTrackState extends State<TaskTrack> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
+      body: CustomCard(
+        margin: const EdgeInsets.symmetric(vertical: 0.0),
+        padding:
+        //AppSpaces.calculatePaddingFromScreenWidth(context),
+        const EdgeInsets.all(15),
+        elevation: 0.0,
+        borderRadius: BorderRadius.circular(15),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40.0,horizontal: 4.0),
           child: Column(
             children: [
+              // Title Section with Button
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Expanded(
                     child: Text(
-                      'My Tasks',
-                      style: TextStyle(color: color.secondaryColor, fontSize: 30),
+                      'My Task',
+                      style: TextStyle(
+                        color: AppColors.secondaryColor,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: AppColors.secondaryColor, width: 2),
+                    ),
                     child: IconButton(
                       onPressed: () {
                         Navigator.push(
@@ -61,47 +82,49 @@ class _TaskTrackState extends State<TaskTrack> {
                           ),
                         );
                       },
-                      icon: Icon(Icons.add, color: color.secondaryColor),
+                      icon: Icon(
+                        Icons.add,
+                        color: AppColors.secondaryColor,
+                      ),
                     ),
                   ),
                 ],
               ),
-
+              const SizedBox(height:20),
               DataFormat(
                 selectedDate: selectedDate,
                 onDateSelected: _onDateSelected,
               ),
+              const SizedBox(height: 16),
+              // Tasks Display Section
+              SizedBox(
+                height: 600,
+                width: 400,
+                child: BlocBuilder<TaskBloc, TaskState>(
+                  builder: (context, state) {
+                    if (state is TaskLoading) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (state is TaskLoaded) {
+                      final tasksForSelectedDate = state.tasks.where((task) {
+                        DateTime taskDate = _parseTaskDate(task.date);
+                        return _isSameDay(taskDate, _parseTaskDate(selectedDate));
+                      }).toList();
 
-              // Tasks Display
-              BlocBuilder<TaskBloc, TaskState>(
-                builder: (context, state) {
-                  if (state is TaskLoading) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (state is TaskLoaded) {
-                    final tasksForSelectedDate = state.tasks.where((task) {
-                      DateTime taskDate = _parseTaskDate(task.date);
-                      return _isSameDay(taskDate, _parseTaskDate(selectedDate));
-                    }).toList();
-
-                    return tasksForSelectedDate.isEmpty
-                        ? const Center(
-                      child: Text(
-                        "No tasks for this date.",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    )
-                        : Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "You have ${tasksForSelectedDate.length} task.",
-                          style: const TextStyle(
-                            fontSize: 15.0,
-                          ),
+                      return tasksForSelectedDate.isEmpty
+                          ? const Center(
+                        child: Text(
+                          "No tasks for this date.",
+                          style: TextStyle(fontSize: 18),
                         ),
-                        const SizedBox(height: 16),
-                        ...tasksForSelectedDate.map(
-                              (task) => TaskCard(
+                      )
+                          : ListView.builder(
+
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: tasksForSelectedDate.length,
+                        itemBuilder: (context, index) {
+                          final task = tasksForSelectedDate[index];
+                          return TaskCard(
                             title: task.title,
                             description: task.description,
                             date: task.date,
@@ -118,30 +141,25 @@ class _TaskTrackState extends State<TaskTrack> {
                                   ),
                                 );
                               } catch (e) {
-                                ScaffoldMessenger.of(context)
-                                    .showSnackBar(
-                                  SnackBar(
-                                    content: Text(
-                                      "Failed to update task status: $e",
-                                    ),
-                                  ),
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text("Failed to update task status: $e")),
                                 );
                               }
-                            },// category: task.category,
-                          ),
+                            },
+                          );
+                        },
+                      );
+                    } else if (state is TaskError) {
+                      return Center(
+                        child: Text(
+                          state.message,
+                          style: const TextStyle(fontSize: 18),
                         ),
-                      ],
-                    );
-                  } else if (state is TaskError) {
-                    return Center(
-                      child: Text(
-                        state.message,
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                    );
-                  }
-                  return const SizedBox();
-                },
+                      );
+                    }
+                    return const SizedBox();
+                  },
+                ),
               ),
             ],
           ),
@@ -149,6 +167,7 @@ class _TaskTrackState extends State<TaskTrack> {
       ),
     );
   }
+
 
   DateTime _parseTaskDate(String dateString) {
     try {
